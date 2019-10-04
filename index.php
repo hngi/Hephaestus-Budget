2c57bbf3162b63fc67a120bbc6c63db7cf5bc4c8
Theresa Akpan <theresaakpan001@gmail.com>
2019-10-04 07:04:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,13 @@ if (isset($_SESSION['user'])) {
         </button>
         <p class="h3 font-weight-bold text-info m-0" id="heph">HephBudget</p>
 
+       <nav class="navbar navbar-expand-lg hephnav">
+        <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <p class="h3 font-weight-bold text-info m-0" id="heph">HephBudget</p>
+
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
@@ -35,7 +42,7 @@ if (isset($_SESSION['user'])) {
                         role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         About</a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="about.html">About Us</a>
+                        <a class="dropdown-item" href="index.html">About Us</a>
                         <a class="dropdown-item" href="#">FAQ</a>
 
                     </div>
