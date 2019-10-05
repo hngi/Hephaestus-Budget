deb337b1c976ac532d002624992e75b2d787ddb4
Samuel Omanchi <omanchis@gmail.com>
2019-10-05 11:18:53 +0100

    fixed home

--- home.php
+++ home.php
@@ -28,7 +28,37 @@ if (isset($_POST['logout'])) {
 
 <body>
 
-<?php include("partials/navigation.php"); ?>
+<nav class="navbar navbar-expand-lg hephnav">
+        <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <p class="h3 font-weight-bold text-info m-0" id="heph">HephBudget</p>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link font-weight-bold" href="home">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle font-weight-bold" href="#" id="navbarDropdownMenuLink"
+                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        About</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="about">About Us</a>
+                        <a class="dropdown-item" href="faqs">FAQ</a>
+                    </div>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link font-weight-bold contact" href="contact">Contact</a>
+                </li>
+            </ul>
+            <form method="post"><small class=" text-light font-weight-bold position-relative mr-1"	
+                    style="font-size: 14px; top: 1px">Hello <?php echo $user['name'] ?> </small>	
+                <button name="logout" type="submit" class="btn badge badge-danger p-2"> <i	
+                        class="fa fa-power-off"></i></button></form>
+        </div>
+    </nav>
 
     <div class="position-absolute w-100 h-100" style="top: 0">
         <div class="d-md-flex justify-content-center pt-5 my-5">
