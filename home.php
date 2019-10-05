00d2dd324e9d628b0cb4aa4c086cc3205267a233
monique94 <agbomonica.am@gmail.com>
2019-10-05 12:19:20 -0700

    added image feature

--- home.php
+++ home.php
@@ -13,7 +13,6 @@ if (isset($_POST['logout'])) {
     session_destroy();
     header("location: login.php");
 }
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -27,21 +26,42 @@ if (isset($_POST['logout'])) {
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/calculator.css" />
-    <link rel="stylesheet" href="assets/js/bootstrap.min.js" />
-    <link rel="stylesheet" href="assets/js/jquery.min.js" />
 </head>
 
 <body>
-<nav class="navbar head p-0 px-2">
-    <p class="m-0"><a href="./" class="nav-link hephbrand text-light h5 font-weight-bold" style="color: inherit">HephBudget</a></p>
-        <form method="post">
-            
-        <img class="profile-circle" src="<?php echo $imageURL; ?>" alt="User Image"/><span class=""></span>
 
-    <small class=" text-light font-weight-bold position-relative mr-1" style="font-size: 14px; top: 1px">
-    
-    <?php echo $user['name'] ?></small>
-            <button name="logout" type="submit" class="btn badge badge-danger p-2"> <i class="fa fa-power-off"></i></button></form>
+<nav class="navbar navbar-expand-lg hephnav">
+        <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <p class="h3 font-weight-bold text-info m-0" id="heph">HephBudget</p>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link font-weight-bold" href="home">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle font-weight-bold" href="#" id="navbarDropdownMenuLink"
+                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        About</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="about">About Us</a>
+                        <a class="dropdown-item" href="faqs">FAQ</a>
+                    </div>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link font-weight-bold contact" href="contact">Contact</a>
+                </li>
+            </ul>
+            <form method="post">
+
+            <img class="profile-circle" src="<?php echo $imageURL; ?>" alt="User Image"/><span class=""></span>
+            <small class=" font-weight-bold position-relative mr-1" style="font-size: 14px; top: 1px; color:#298b92;"><?php echo $user['name'] ?> </small>	
+                <button name="logout" type="submit" class="btn badge badge-danger p-2"> <i	
+                 class="fa fa-power-off"></i></button></form>
+        </div>
     </nav>
 
     <div class="position-absolute w-100 h-100" style="top: 0">
@@ -107,6 +127,7 @@ if (isset($_POST['logout'])) {
 
 
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+
     <script type="text/javascript" src="assets/js/calculator.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
